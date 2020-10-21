752a1b3d813cd152fd9cc36cfdea37cd9d8fd44e
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-10-22 01:21:18 +0530

    added endpoints

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import requests from './requests'
+import Row from './components/Row'
 
 function App() {
   return (
     <div className="App">
       <h1>Hey, starting the netflix-clone</h1>
+      <Row title='Netflix Originals' fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title ='Trending Now' fetchUrl = {requests.fetchTrending}/>
     </div>
   );
 }
