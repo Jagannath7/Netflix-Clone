b8d70d1776041f96814873450a8553c23eb2dea7
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-10-22 14:58:16 +0530

    changes posters

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import Row from './components/Row'
 function App() {
   return (
     <div className="App">
-      <h1>Hey, starting the netflix-clone</h1>
-      <Row title='Netflix Originals' fetchUrl={requests.fetchNetflixOriginals} />
+      {/* navbar-component */}
+      {/* banner-component */}
+      <Row title='Netflix Originals' fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
       <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
       <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
